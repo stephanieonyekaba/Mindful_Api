29d7dd986e9b99a139f3a5919d850d30280b0321
stephanieonyekaba <onyek009@umn.edu>
2022-04-24 00:54:05 -0400

    all examples removed from template

--- mongoose-express-auth-API-boilerplate-main/server.js
+++ mongoose-express-auth-API-boilerplate-main/server.js
@@ -4,7 +4,6 @@ const mongoose = require('mongoose')
 const cors = require('cors')
 
 // require route files
-const exampleRoutes = require('./app/routes/example_routes')
 const yogaRoutes = require('./app/routes/yoga_routes')
 const affirmationRoutes = require('./app/routes/affirmation_routes')
 const userRoutes = require('./app/routes/user_routes')
@@ -67,7 +66,6 @@ app.use(express.urlencoded({ extended: true }))
 app.use(requestLogger)
 
 // register route files
-app.use(exampleRoutes)
 app.use(userRoutes)
 app.use(yogaRoutes)
 app.use(affirmationRoutes)
