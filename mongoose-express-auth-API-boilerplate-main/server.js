2fc42512f8718f50cc970bc8b89f731fc3d485b7
stephanieonyekaba <onyek009@umn.edu>
2022-04-24 00:46:58 -0400

    yoga index route functioning with server.js requirement ment

--- mongoose-express-auth-API-boilerplate-main/server.js
+++ mongoose-express-auth-API-boilerplate-main/server.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 
 // require route files
 const exampleRoutes = require('./app/routes/example_routes')
+const yogaRoutes = require('./app/routes/yoga_routes')
 const userRoutes = require('./app/routes/user_routes')
 
 // require middleware
@@ -67,6 +68,7 @@ app.use(requestLogger)
 // register route files
 app.use(exampleRoutes)
 app.use(userRoutes)
+app.use(yogaRoutes)
 
 // register error handling middleware
 // note that this comes after the route middlewares, because it needs to be
