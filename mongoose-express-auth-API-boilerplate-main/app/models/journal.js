a982f0c57508821d9014a6454de930f4f227cdc9
stephanieonyekaba <onyek009@umn.edu>
2022-04-25 13:15:32 -0400

    journal schema created

---
+++ mongoose-express-auth-API-boilerplate-main/app/models/journal.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+const { Schema, model } = mongoose
+const journalSchema = new mongoose.Schema(
+	{
+        date: {
+			type: Date,
+			required: true,
+		},
+        entry: {
+			type: String,
+			required: true,
+		},
+		owner: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'User',
+			required: false,
+		},
+	},
+	{
+	timestamps: true,
+	}
+	)
+
+module.exports = mongoose.model('Journal', journalSchema)
